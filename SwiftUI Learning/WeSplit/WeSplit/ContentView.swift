fd0af49b867bd29e44940ff331244a4c8f743748
Sarunyu Prasert <sarunyu.prasert@gmail.com>
2026-01-09 01:36:18 +0700

    Edit code

---
+++ SwiftUI Learning/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sarunyu Prasert on 24/6/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.brown)
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
